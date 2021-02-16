da68142d786661b972343ae7871cbdf7bc172873
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-16 14:09:34 -0600

    Ruta 404 con animaciones

---
+++ src/components/Main.svelte
@@ -0,0 +1,26 @@
+<main class="Main">
+  <div class="Main-container">
+    <div class="Main-content">
+      <slot />
+      <div class="Sidebar">Sidebar </div>
+    </div>
+  </div>
+</main>
+
+<style>
+  .Main-container {
+    display: grid;
+    grid-template-columns: minmax(auto, 1024px);
+    justify-content: center;
+    align-items: center;
+  }
+
+  .Main-content {
+    margin: 2em 0 0;
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 68px;
+    justify-content: space-between;
+  }
+
+</style>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+	import Main from '../components/Main.svelte';
+
+	export let segment;
+</script>
+
+<Nav {segment}/>
+
+<Main>
+	<slot></slot>
+</Main>
