425d37096ff9ec2fc37662bb4cf3a37798ba547f
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-17 12:44:56 -0600

    Metadatos y optimización del SEO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,6 +15,25 @@
 
 <svelte:head>
   <title>Eric Gomez</title>
+  <meta name="description" content="Blog de ericgomez live">
+  
+  <meta name="twitter:card" content="summary_larga_image">
+  <meta name="twitter:site" content="@ericgomezmx">
+  <meta name="twitter:creator" content="@ericgomezmx">
+  <meta name="twitter:title" content="Eric Gomez">
+  <meta name="twitter:description" content="Blog de ericgomez live">
+  <meta name="twitter:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png">
+
+  <meta name="og:title" content="Eric Gomez">
+  <meta name="og:description" content="Blog de ericgomez live">
+  <meta name="og:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png">
+  <meta name="og:url" content="https://blog.ericgomez.live">
+  <meta name="og:site_name" content="es_ES">
+  <meta name="og:locale" content="Eric Gomez">
+  <meta name="og:type" content="article">
+  <meta name="fb:app_id" content="Eric Gomez">
+
+
 </svelte:head>
 
 <div class="Home">
