29db160de2e3c78dd452f9a2ff02affe073ffbc1
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-17 11:59:30 -0600

    Terminando la integración

--- src/routes/general.svelte
+++ src/routes/general.svelte
@@ -1,7 +1,42 @@
+<script context="module">
+  export function preload() {
+    return this.fetch(`blog.json`)
+      .then((r) => r.json())
+      .then((posts) => {
+        return { posts };
+      });
+  }
+</script>
+
+<script>
+  import Post from './../components/Post';
+  export let posts;
+  const filterPost = posts.filter(post => post.tag === 'general');
+</script>
+
 <svelte:head>
   <title>General</title>
 </svelte:head>
 
 <div class="General">
-  <h2>General</h2>
+  <h1>General</h1>
+  <div class="Posts">
+    {#if filterPost.length >= 1}
+      {#each filterPost as post}
+         <Post {post} />
+      {/each}
+    {:else}
+      <p>Sin resultados...</p>
+    {/if}
+  </div>
 </div>
+
+
+<style>
+  .Posts {
+    display: grid;
+    justify-content: space-between;
+    gap: 30px;
+    grid-template-columns: 1fr;
+  }
+</style>
